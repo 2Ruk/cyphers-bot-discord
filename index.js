66656d2c8cd006602df9e543e7285c3baba7713f
IanChoi0122 <110802596+IanChoi0122@users.noreply.github.com>
2023-02-26 14:58:48 +0900

    feat: project setting

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('index.js 실행 되었습니다.')
+import * as Discord from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: '.env'
+});
+
+const discord_token = process.env.DISCORD_TOKEN
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(discord_token);
